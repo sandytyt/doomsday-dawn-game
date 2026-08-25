ee3bf10de99d78e87de647d9599623ae1e46c814
sandytyt <slysandy2001@gmail.com>
2026-08-25 11:21:54 +0800

    Update app-npc-state.js

--- app-npc-state.js
+++ app-npc-state.js
@@ -1,1 +1,17 @@
-//tbc
+'use strict';
+
+/**
+ * 宣告 npcStates 結構與函式骨架
+ */
+
+function createNpcStateSkeleton(npcName) {
+  // 階段2 將實作初始化邏輯
+}
+
+function correctNpcStateOnRejoin(npcName, currentTurn) {
+  // 階段2 將實作離隊超過 15 回合的校正邏輯
+}
+
+function clearNpcStateOnDeath(npcName) {
+  // 階段6 將實作死亡清除邏輯
+}
